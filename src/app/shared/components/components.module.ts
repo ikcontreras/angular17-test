7ef3134185f8fcbe3acc75fd2fd7684e551272fc
Israel Contreras <israel.contreras@bosonit.com>
2023-11-10 17:07:16 +0100

    feat: create module component of shared folder

---
+++ src/app/shared/components/components.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { ItemComponent } from './Item/Item.component';
+
+@NgModule({
+  declarations: [
+    ItemComponent
+  ],
+  exports: [
+    ItemComponent
+  ]
+})
+export class ComponentsModule { }
